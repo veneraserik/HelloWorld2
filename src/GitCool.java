3127f16628811a103fc61e4c59bc965d4e612341
B23-User <venera.abulkhair@gmail.com>
2021-08-01 08:45:52 +0600

    added Github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,5 @@ public class GitCool {
     //B23 is learning git
     //B23 is makes some sense now
    // We added out Github account
+    // we were able to successfully shared project under github account
 }
